e625f9154348b3e4bf3cc82e1f60e7c61a4f9633
KIPKOPCT <kipkopctdev@gmail.com>
2025-11-11 15:53:25 +0000

    tuesdays

--- main.js
+++ main.js
@@ -35,3 +35,23 @@ const dot = document.getElementById('dot');
       requestAnimationFrame(animate);
     }
     animate();
+
+
+
+    
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+  const question = item.querySelector(".faq-question");
+  question.addEventListener("click", () => {
+    faqItems.forEach(i => {
+      if (i !== item) {
+        i.classList.remove("active");
+        i.querySelector(".faq-answer").classList.remove("open");
+      }
+    });
+
+    item.classList.toggle("active");
+    item.querySelector(".faq-answer").classList.toggle("open");
+  });
+});
